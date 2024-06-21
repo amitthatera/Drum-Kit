fdf09f9eb38033bf2c6b35685209cb1a6db7e21c
Amit Thatera <amitthatera.dev@gmail.com>
2024-06-21 19:25:41 +0530

    changes in index.js

--- index.js
+++ index.js
@@ -1,36 +1,19 @@
 
 function playSound(key) {
-    switch (key) {
-        case 'w':
-            let tom1 = new Audio('sounds/tom-1.mp3');
-            tom1.play();
-            break;
-        case 'a':
-            let tom2 = new Audio('sounds/tom-2.mp3');
-            tom2.play();
-            break;
-        case 's':
-            let tom3 = new Audio('sounds/tom-3.mp3');
-            tom3.play();
-            break;
-        case 'd':
-            let tom4 = new Audio('sounds/tom-4.mp3');
-            tom4.play();
-            break;
-        case 'j':
-            let kick = new Audio('sounds/kick-bass.mp3');
-            kick.play();
-            break;
-        case 'k':
-            let snare = new Audio('sounds/snare.mp3');
-            snare.play();
-            break;
-        case 'l':
-            let crash = new Audio('sounds/crash.mp3');
-            crash.play();
-            break;
-        default:
-            break;   
+    let soundMap = {
+        'w': 'tom-1.mp3',
+        'a': 'tom-2.mp3',
+        's': 'tom-3.mp3',
+        'd': 'tom-4.mp3',
+        'j': 'kick-bass.mp3',
+        'k': 'snare.mp3',
+        'l': 'crash.mp3'
+    };
+    
+    let soundFile = soundMap[key];
+    if (soundFile) {
+        let audio = new Audio('sounds/' + soundFile);
+        audio.play();
     }
 }
 
